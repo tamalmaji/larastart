cbb5e0fa6485d4d47981dd65dda863e98ce02067
Your Name <you@example.com>
2021-10-04 21:24:53 +0530

    4.10 3.24

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,8 +12,24 @@ import Form from 'vform';
 import Vue from 'vue';
 window.Form = Form;
 // import Vue from 'vue'
+import VueProgressBar from 'vue-progressbar'
 import router from './router'
+import Swal from 'sweetalert2'
+window.Swal = Swal;
 
+const toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+window.toast = toast;
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -32,6 +48,25 @@ Vue.filter('upText', function(text){
 Vue.filter('myDate', function(created) {
     return moment(created).format('MMMM Do YYYY');
 });
+
+const options = {
+    color: '#bffaf3',
+    failedColor: '#874b4b',
+    thickness: '5px',
+    transition: {
+      speed: '0.2s',
+      opacity: '0.6s',
+      termination: 300
+    },
+    autoRevert: true,
+    location: 'top',
+    inverse: false
+  }
+  
+  Vue.use(VueProgressBar, options)
+
+  window.Fire =  new Vue();
+  
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -35,7 +35,7 @@
                                <i class="fa fa-edit blue"></i>
                           </a>
                           /
-                          <a href="#">
+                          <a href="#" @click="deleteUser(user.id)">
                                <i class="fa fa-trash red"></i>
                           </a>
                           
@@ -128,16 +128,60 @@
             }
         },
         methods: {
+        deleteUser(id){
+          Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            // Send request to the server
+            this.form.delete('api/user/'+id).then(()=>{
+               if (result.isConfirmed) {
+                  Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                  )
+                  Fire.$emit('AfterCreate');
+                }
+            }).catch(()=>{
+              swal("Failed!", "There was something wronge.", "warning");
+            })
+           
+          })
+        },
         loadUsera(){
             axios.get("/api/user").then(({ data }) => (this.users = data)) 
           },
          async createUser(){
+            this.$Progress.start()
             const response = await this.form.post('/api/user')
+            .then(()=>{
+                  Fire.$emit('AfterCreate');
+                $('#addNew').modal('hide')
+                toast.fire({
+                  icon: 'success',
+                  title: 'User Created successfully'
+                })
+                this.$Progress.finish()
+            })
+            .catch(()=>{
+              
+            })
             
           },
         },
         created() {
            this.loadUsera();
+           
+           Fire.$on('AfterCreate', () => {
+           this.loadUsera();
+           });
+            // setInterval(() => this.loadUsera(), 3000);
         }
     }
 </script>
